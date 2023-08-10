2c2f908e1d254c32a47cd9765c1201702814406f
Rizvan <imeandmyep2@gmail.com>
2023-08-10 16:30:21 +0300

    yandex auth, next-auth.d

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import YandexProvider from "next-auth/providers/yandex";
 import { NextAuthOptions } from "next-auth";
+import { nanoid } from "nanoid";
 import { db } from "./db";
 
 export const authOptions: NextAuthOptions = {
@@ -17,4 +18,48 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+      if (!dbUser) {
+        token.id = user!.id;
+        return token;
+      }
+      if (!dbUser.username) {
+        await db.user.update({
+          where: {
+            id: dbUser.id,
+          },
+          data: {
+            username: nanoid(10),
+          },
+        });
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+        username: dbUser.username,
+      };
+    },
+    redirect() {
+      return "/";
+    },
+  },
 };
